7e33cbbe24853e7056ec519a5857cc54640dbb92
kiboma <omayioben@gmail.com>
2024-03-04 18:46:21 +0300

    Style footer Style main page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>
-        <h1>Footer</h1>
+    <div className='flex justify-center items-center font-bold'>
+        <h1>© 2030 WriteNode. All Rights Reserved.</h1>
       
     </div>
   )

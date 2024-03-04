88fbe8a174d647707d623ae135d175dcfb922bde
kiboma <omayioben@gmail.com>
2024-03-04 18:23:33 +0300

    Style header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link,NavLink } from 'react-router-dom'
+import Logo from '../assets/logo.jpeg'
 
 export const Header = () => {
   return (
     <div>
         <Link>
-            <img src="" alt="" />
+            <img src={Logo} alt="NoteBook" />
             NoteBook
         </Link>
         <div>

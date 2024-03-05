e63db0787b90c7e02841f05731dc492e6fd79383
kiboma <omayioben@gmail.com>
2024-03-05 07:57:33 +0300

    Toggle dark mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link,NavLink } from 'react-router-dom'
 import Logo from '../assets/logo.jpeg'
 
 export const Header = () => {
-  const [darkMode, setdarkMode ] = useState(false);
+  const [darkMode, setdarkMode ] = useState(JSON.parse(localStorage.getItem("darkMode")) || false);
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    if (darkMode){
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  },[darkMode]);
   return (
-    <div className='flex justify-between items-center m-10'>
+    <div className='flex justify-between items-center flex-wrap m-10 dark:bg-slate-900 dark:text-white'>
         <Link to='/' className='flex gap-3 items-center text-3xl '>
             <img className='h-12' src={Logo} alt="NoteBook" />
             NoteBook
         </Link>
         <div className='text-2xl'>
-            <span>{darkMode?(<i class="bi bi-sun-fill mx-2" />):(<i className="bi bi-moon-stars-fill mx-2" />)}
+            <span onClick={()=>setdarkMode(!darkMode)}>{darkMode?(<i class="bi bi-sun-fill mx-2" />):(<i className="bi bi-moon-stars-fill mx-2" />)}
               
             </span>
             <NavLink to='/' className='mx-2'>Home</NavLink>

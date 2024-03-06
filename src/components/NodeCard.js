e35f8825d8484bb8ab0186f5fe7ccf0a0671de41
kiboma <omayioben@gmail.com>
2024-03-06 08:59:23 +0300

    Stle home page

--- src/components/NodeCard.js
+++ src/components/NodeCard.js
@@ -8,9 +8,9 @@ export const NodeCard = ({post}) => {
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{post.title}</h5>
           <p className="font-normal text-gray-700 dark:text-gray-400">{post.body}</p>
           <div className="flex flex-wrap gap-5 mt-5">
-            <span className="bg-gray-300 mr-5 p-2 rounded-xl dark:bg-blue-600">Posted By: Ben Kiboma</span>
-            <span className="bg-gray-300 mr-5 p-2 rounded-xl dark:bg-blue-600">Date Created: Ben Kiboma</span>
-            <span className="bg-gray-300 mr-5 p-2 rounded-xl dark:bg-blue-600">Date Updated: Ben Kiboma</span>
+            <span className="bg-green-300 text-wh mr-5 p-2 rounded-xl dark:bg-green-600">Posted By: Ben Kiboma</span>
+            <span className="bg-blue-300 mr-5 p-2 rounded-xl dark:bg-blue-600">Date Created: Ben Kiboma</span>
+            <span className="bg-yellow-300 mr-5 p-2 rounded-xl dark:bg-yellow-600">Date Updated: Ben Kiboma</span>
           </div>         
         </div>
         <div className="flex flex-col justify-end p-4">

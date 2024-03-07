549228f2c1173ce88cf6b3e831ba26cab477f08f
kiboma <omayioben@gmail.com>
2024-03-07 16:40:59 +0300

    Add protected routes

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,11 +1,12 @@
 import { Routes,Route } from 'react-router-dom';
 import { Home,CreatePost,PageNotFound } from '../pages';
+import { ProtectedRoutes } from './ProtectedRoutes';
 
 export const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Home title='All Posts' />} />
-        <Route path='/post' element={<CreatePost title='Add Post' />} />
+        <Route path='/post' element={<ProtectedRoutes><CreatePost title='Add Post' /></ProtectedRoutes> } />
         <Route path='*' element={<PageNotFound title='404!' />} />
       
     </Routes>

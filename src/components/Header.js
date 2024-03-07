ba2be30e1f60f1191e80daf3ec72194c2682762c
kiboma <omayioben@gmail.com>
2024-03-07 08:51:34 +0300

    Delete useState

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { Link,NavLink } from 'react-router-dom'
 import Logo from '../assets/logo.jpeg'
 

8b3f8251e778f46367bcf2fa679b63c00f0b5b51
kiboma <omayioben@gmail.com>
2024-03-07 10:09:42 +0300

    Configure and fetch data from firebase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase/config"
 import { NodeCard } from "../components";
-export const Home = () => {
 
-  const posts = [
-    { title: 'Lorem ipsum dolor sit', body: 'Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet. Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet.'},
-    { title: 'Lorem ipsum dolor sit ametLorem ipsum dolor', body: 'Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet'},
-    { title: 'title', body: 'body'}
-  ];
+
+export const Home = () => {
+  const [posts, setPosts ] = useState([])
+  const postsRef = collection(db, "posts");
+
+  useEffect(() => {
+    async function getPosts() {
+      const data = await getDocs(postsRef);
+      setPosts(data.docs.map((document) => (
+        {...document.data(),id: document.id}
+      )
+      ));
+    }
+    getPosts()
+  },[]);
+
+  // const posts = [
+  //   { title: 'Lorem ipsum dolor sit', body: 'Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet. Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet.'},
+  //   { title: 'Lorem ipsum dolor sit ametLorem ipsum dolor', body: 'Lorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit ametLorem ipsum dolor sit amet'},
+  //   { title: 'title', body: 'body'}
+  // ];
   return (
     <main>
       <div>

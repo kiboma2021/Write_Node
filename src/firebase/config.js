28195e254d68f139039bbc84f374a12f2909c26f
kiboma <omayioben@gmail.com>
2024-03-08 18:22:11 +0300

    set up env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBshTzWsNfPs6Wkrm4BL6qAIaqXzUqBCGY",
-  authDomain: "writenode-4b860.firebaseapp.com",
-  projectId: "writenode-4b860",
-  storageBucket: "writenode-4b860.appspot.com",
-  messagingSenderId: "430975369990",
-  appId: "1:430975369990:web:17834caf0f169334215487"
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN }`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID }`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET }`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID }`,
+  appId: `${process.env.REACT_APP_APP_ID }`
 };
 
 // Initialize Firebase
